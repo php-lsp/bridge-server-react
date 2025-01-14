fb6c5283041f4953d0168fadf6d82faba8d7fe42
SerafimArts <nesk@xakep.ru>
2025-01-14 03:05:28 +0000

    Add type-lang mapper v0.3 support

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lsp\Server\Bridge\React\Tests;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+#[Group('php-lsp/bridge-server-react')]
+abstract class TestCase extends BaseTestCase {}
